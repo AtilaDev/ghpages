97ec90a5cf45e9412753cafc2f9f433e362861f8
Leandro Favre <atiladevelop@gmail.com>
2024-11-10 03:09:35 -0300

    Intial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/ghpages",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
